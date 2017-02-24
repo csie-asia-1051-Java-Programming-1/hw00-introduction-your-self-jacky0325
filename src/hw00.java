18c517a83c8866387f19455eea0d9366f5de941e
jacky0325 <105021051@live.asia.edu.tw>
2017-02-24 15:02:26 +0800

    hw00 submit02

--- src/hw00.java
+++ src/hw00.java
@@ -1,10 +1,13 @@
 	/*
+	* Topic: 個人資料
 	* Date: 2017/02/24
+	* Author: 105021051 郭家源
 	*/
 public class hw00 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.print("資工系 1A 資電組 \n105021051\n郭家源");
 	}
 
 }
